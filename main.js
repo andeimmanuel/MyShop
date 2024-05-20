449b134f977db5636445980b26acb216b7fb6da6
ande <andeimmanuel@gmail.com>
2024-05-20 23:21:27 +0100

    My first commit on this project

---
+++ main.js
@@ -0,0 +1,51 @@
+const triggerOpen = document.querySelectorAll("[trigger-button]");
+const triggerClose = document.querySelectorAll("[close-button]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < triggerOpen.length; i++) {
+  let currentId = triggerOpen[i].dataset.target,
+    targetEl = document.querySelector(`#${currentId}`);
+
+  const openData = function () {
+    targetEl.classList.remove("active");
+    overlay.classList.remove("active");
+  };
+
+  triggerOpen[i].addEventListener("click", function () {
+    targetEl.classList.add("active");
+    overlay.classList.add("active");
+  });
+
+  targetEl.querySelector("[close-button]").addEventListener("click", openData);
+  overlay.addEventListener("click", openData);
+}
+
+// mobile-menu submenu
+
+const submenu = document.querySelectorAll(".child-trigger");
+submenu.forEach((menu) =>
+  menu.addEventListener("click", function (e) {
+    e.preventDefault();
+    submenu.forEach((item) =>
+      item != this
+        ? item.closest(".has-child").classList.remove("active")
+        : null
+    );
+    if (this.closest(".has-child").classList != "active") {
+      this.closest(".has-child").classList.toggle("active");
+    }
+  })
+);
+
+// slider
+
+const swiper = new Swiper(".sliderbox", {
+  loop: true,
+  effect: "fade",
+  autoHeight: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
