2b6a6331e05fe8330d927135c3194b5629640482
ande <andeimmanuel@gmail.com>
2024-05-23 17:27:04 +0100

    added my second page

--- main.js
+++ main.js
@@ -135,3 +135,27 @@ const carousel = new Swiper(".carouselbox", {
     },
   },
 });
+
+// Product image
+
+const thumbImage = new Swiper(".thumbnail-image", {
+  // loop: true,
+  direction: "vertical",
+  spaceBetween: 15,
+  slidesPerView: 1,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const mainImage = new Swiper(".main-image", {
+  loop: true,
+  autoHeight: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  thumbs: {
+    swiper: thumbImage,
+  },
+});
